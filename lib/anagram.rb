e55afe32ea63c2878d1c61698605283714592d8a
Sandie Nunez <snunez@vt.edu>
2020-04-19 01:42:30 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -5,7 +5,7 @@ class Anagram
   end 
   
   def match(array_of_words) 
-    array_of_words.keep_if {|str| word.split('').sort == str.split('').sort
+    array_of_words.keep_if {|str| word.split('').sort == str.split('').sort}
   # array_of_words.select do |element|
   #     (@word.split("").sort) == (element.split("").sort)
     end
